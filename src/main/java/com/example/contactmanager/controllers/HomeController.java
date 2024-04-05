3a2db93f98cbe56ebbe81c616317a9cac0b70532
Lajpat Rai <lajpatsankhla@gmail.com>
2024-04-06 00:35:18 +0500

    registration completed

--- src/main/java/com/example/contactmanager/controllers/HomeController.java
+++ src/main/java/com/example/contactmanager/controllers/HomeController.java
@@ -4,10 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.contactmanager.dao.UserRepository;
 import com.example.contactmanager.entities.User;
+import com.example.contactmanager.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -43,7 +50,34 @@ public class HomeController {
     @GetMapping("/signup")
     public String signUp(Model model) {
         model.addAttribute("title", "signUp -contact Manager");
+        model.addAttribute("user", new User());
         return "signup";
+    }
+    //handler for registering user
+    @PostMapping("/do-register")
+    public String registerUser(@ModelAttribute("user") User in_user,@RequestParam(value = "check1",defaultValue = "false")boolean check1,Model model,HttpSession session){
+        try {
+            if(!check1){
+                System.out.println("not agreed term and conditions...");
+                throw new Exception("not agggreed terms and conditions");
+            }
+            in_user.setRole("admin");
+            in_user.setEnable(true);
+            User result=userRepository.save(in_user);
+            System.out.println("Agreement:"+check1);
+            System.out.println("User:"+in_user);
+            model.addAttribute("user", new User());
+            session.setAttribute("message", new Message("Succesfully registered...", "alert-success"));
+            return "signup";
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("user", in_user);  //again sending to page to show on fields as previous value
+            session.setAttribute("message", new Message("something wents wrong..."+ e.getMessage(), "alert-danger"));
+            return "signup";
+        }
+
+        
     }
     
 }
